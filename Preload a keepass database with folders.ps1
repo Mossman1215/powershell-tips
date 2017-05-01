2ed7c3f6b9e705a7a150e7221cbb59312977e615
Moss Canwell <mossman2142@gmail.com>
2017-05-01 14:09:11 +1200

    Added preload keepass script
    A script that will add folders to a keepass database based on a text file.
    I used this to automate a transition to keepass for confidential information

---
+++ Preload a keepass database with folders.ps1
@@ -0,0 +1,23 @@
+﻿function KPS-Dir{
+    param([String]$name,[String]$root)
+    if($root.Length -eq 0){
+        .\KPScript.exe -c:AddEntry O:\Passwords\Caduceus.kdbx -pw:$dbpwd -Title:'test' -GroupPath:"Clients/$name"
+    }else{
+        .\KPScript.exe -c:AddEntry O:\Passwords\Caduceus.kdbx -pw:$dbpwd -Title:'test' -GroupPath:"Clients/$root/$name"
+    }
+}
+$file = Get-Content C:\Users\moss\Documents\Dev\clients.txt
+Set-Location -Path "C:\Program Files (x86)\KeePass Password Safe 2"
+foreach ($name in $file){
+    if($name.length -eq 1){
+        #remember name as $rootdir
+        $rootdir = $name
+    }
+    if(!$name.Contains('.') ){
+        if($name -eq $rootdir){
+            KPS-Dir -name $name -root ""
+        }else{
+            KPS-Dir -name $name -root $rootdir
+        }
+    }
+}
