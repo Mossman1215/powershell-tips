9bacb3d515adb4f6fe7cb4e6ae6c372c30631495
Moss Canwell <mossman2142@gmail.com>
2017-05-01 14:31:43 +1200

    Added extra commands

--- Commands.ps1
+++ Commands.ps1
@@ -12,3 +12,15 @@ $credential = get-credential -message "Password for system" -username "moss"
 cat .\programssort.txt | foreach{$filter = "Name = '"+ $_+"'"; $app = Get-WMIobject -class Win32_Product -filter $filter; $app.uninstall() }
 #map a drive
 New-PSDrive -Persist -Name Z -PSProvider FileSystem -Root $folder
+
+#set active hours registry values
+Set-ItemProperty HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name ActiveHoursStart -Value 8
+Set-ItemProperty HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name ActiveHoursEnd -Value 20
+
+#enable .net3
+enable-WindowsOptionalFeature -Online -PackageName netfx3
+
+#run a program once on next boot
+$exePath = 'C:\Temp\update.exe'
+$RunOnceKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+Set-ItemProperty $RunOnceKey "NextRun" ($exePath)
